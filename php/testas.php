7be2dce0c72d52f4cfc47cfd24dd5598863444d7
vitaliykyq <37109256+vitaliykyq@users.noreply.github.com>
2018-03-11 22:15:15 +0300

    1234
    123

---
+++ php/testas.php
@@ -0,0 +1,14 @@
+<?php
+$db = mysql_connect('localhost','vitaliykyq','ginumu21');
+mysql_select_db('test',$db);
+mysql_query ("set_client='utf8'");
+
+mysql_query ("set character_set_results='utf8'");
+
+mysql_query ("set collation_connection='utf8_general_ci'");
+
+mysql_query ("SET NAMES utf8");
+
+?>
+
+?>
